5bce29ca05bca65e45c98459832eb9373884a9ff
Sinthujan <sthana@devboost.de>
2020-10-08 09:33:11 +0200

    Extended functionality of module....

--- car_seller.py
+++ car_seller.py
@@ -1,12 +1,21 @@
 # imports the class Car from the file car.py
 from car import Car
 
-# returns a new Car instance
-def sellBMW():
-    return Car("BMW", 5, 300)
+class CarSeller:
+    def __init__(self):
+        self.inventory = []
+    # returns a new Car instance
+    def sellBMW():
+        return Car("BMW", 5, 300)
 
-# modifies instance variable of a given car
-def upgradeCar(car):
-    # calling an instance function and accessing instance variables
-    car.updateEngine(car.hp + 100)
-    return car
+    # modifies instance variable of a given car
+    def upgradeCar(car):
+        # calling an instance function and accessing instance variables
+        car.updateEngine(car.hp + 100)
+        return car
+
+    def buyCar(self, car):
+        self.inventory.append(car)
+        
+    def sellCar(self):
+        return self.inventory[-1]
